2f0e977f5aa1a842dfe1d144112316068cc1143e
unknown <emailerdinesh@gmail.com>
2018-09-28 13:25:30 +0530

    Dependancies, folder structure and work flow set-up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./containers/App";
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import registerServiceWorker from "./registerServiceWorker";
+import "./css/main.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialstate = {};
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  initialstate,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  test: "hi can you see me"
+});
+
+export default rootReducer;
